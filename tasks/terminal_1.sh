5022493a61f95776eb7949bd321c7f425899464d
Anonymeee <1712543411@qq.com>
2025-11-06 17:05:27 +0800

    update

--- tasks/terminal_1.sh
+++ tasks/terminal_1.sh
@@ -1,63 +1,183 @@
 #!/bin/bash
-# Terminal 2 tasks
+# Terminal 2 tasks (GPU 1) - Total: 60 tasks
 
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 fedpgp 8 0.0 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 fedpgp 8 0.01 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 fedpgp 8 0.05 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 fedpgp 8 0.1 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 fedpgp 8 0.2 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 fedpgp 8 0.4 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedpgp 8 0.0 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedpgp 8 0.01 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedpgp 8 0.05 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedpgp 8 0.1 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedpgp 8 0.2 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedpgp 8 0.4 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/caltech101.yaml 10 promptfl 8 0.0 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/caltech101.yaml 10 promptfl 8 0.01 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/caltech101.yaml 10 promptfl 8 0.05 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/caltech101.yaml 10 promptfl 8 0.1 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/caltech101.yaml 10 promptfl 8 0.2 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/caltech101.yaml 10 promptfl 8 0.4 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 promptfl 8 0.0 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 promptfl 8 0.01 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 promptfl 8 0.05 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 promptfl 8 0.1 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 promptfl 8 0.2 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 promptfl 8 0.4 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 promptfl 8 0.0 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 promptfl 8 0.01 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 promptfl 8 0.05 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 promptfl 8 0.1 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 promptfl 8 0.2 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 promptfl 8 0.4 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 promptfl 8 0.0 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 promptfl 8 0.01 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 promptfl 8 0.05 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 promptfl 8 0.1 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 promptfl 8 0.2 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 promptfl 8 0.4 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/caltech101.yaml 10 fedotp 8 0.0 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/caltech101.yaml 10 fedotp 8 0.01 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/caltech101.yaml 10 fedotp 8 0.05 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/caltech101.yaml 10 fedotp 8 0.1 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/caltech101.yaml 10 fedotp 8 0.2 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/caltech101.yaml 10 fedotp 8 0.4 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 fedotp 8 0.0 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 fedotp 8 0.01 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 fedotp 8 0.05 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 fedotp 8 0.1 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 fedotp 8 0.2 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 fedotp 8 0.4 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 fedotp 8 0.0 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 fedotp 8 0.01 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 fedotp 8 0.05 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 fedotp 8 0.1 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 fedotp 8 0.2 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 fedotp 8 0.4 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedotp 8 0.0 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedotp 8 0.01 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedotp 8 0.05 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedotp 8 0.1 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedotp 8 0.2 1
-bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedotp 8 0.4 1
+# Task 2/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/caltech-101.yaml 10 dpfpl 8 0.0 1 1
+
+# Task 4/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/caltech-101.yaml 10 promptfl 8 0.0 1 1
+
+# Task 6/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 sepfpl 8 0.0 1 1
+
+# Task 8/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 fedpgp 8 0.0 1 1
+
+# Task 10/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 fedotp 8 0.0 1 1
+
+# Task 12/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 dpfpl 8 0.0 1 1
+
+# Task 14/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 promptfl 8 0.0 1 1
+
+# Task 16/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 sepfpl 8 0.0 1 1
+
+# Task 18/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedpgp 8 0.0 1 1
+
+# Task 20/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedotp 8 0.0 1 1
+
+# Task 22/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/caltech-101.yaml 10 dpfpl 8 0.01 1 1
+
+# Task 24/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/caltech-101.yaml 10 promptfl 8 0.01 1 1
+
+# Task 26/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 sepfpl 8 0.01 1 1
+
+# Task 28/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 fedpgp 8 0.01 1 1
+
+# Task 30/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 fedotp 8 0.01 1 1
+
+# Task 32/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 dpfpl 8 0.01 1 1
+
+# Task 34/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 promptfl 8 0.01 1 1
+
+# Task 36/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 sepfpl 8 0.01 1 1
+
+# Task 38/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedpgp 8 0.01 1 1
+
+# Task 40/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedotp 8 0.01 1 1
+
+# Task 42/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/caltech-101.yaml 10 dpfpl 8 0.05 1 1
+
+# Task 44/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/caltech-101.yaml 10 promptfl 8 0.05 1 1
+
+# Task 46/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 sepfpl 8 0.05 1 1
+
+# Task 48/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 fedpgp 8 0.05 1 1
+
+# Task 50/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 fedotp 8 0.05 1 1
+
+# Task 52/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 dpfpl 8 0.05 1 1
+
+# Task 54/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 promptfl 8 0.05 1 1
+
+# Task 56/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 sepfpl 8 0.05 1 1
+
+# Task 58/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedpgp 8 0.05 1 1
+
+# Task 60/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedotp 8 0.05 1 1
+
+# Task 62/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/caltech-101.yaml 10 dpfpl 8 0.1 1 1
+
+# Task 64/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/caltech-101.yaml 10 promptfl 8 0.1 1 1
+
+# Task 66/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 sepfpl 8 0.1 1 1
+
+# Task 68/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 fedpgp 8 0.1 1 1
+
+# Task 70/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 fedotp 8 0.1 1 1
+
+# Task 72/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 dpfpl 8 0.1 1 1
+
+# Task 74/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 promptfl 8 0.1 1 1
+
+# Task 76/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 sepfpl 8 0.1 1 1
+
+# Task 78/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedpgp 8 0.1 1 1
+
+# Task 80/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedotp 8 0.1 1 1
+
+# Task 82/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/caltech-101.yaml 10 dpfpl 8 0.2 1 1
+
+# Task 84/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/caltech-101.yaml 10 promptfl 8 0.2 1 1
+
+# Task 86/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 sepfpl 8 0.2 1 1
+
+# Task 88/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 fedpgp 8 0.2 1 1
+
+# Task 90/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 fedotp 8 0.2 1 1
+
+# Task 92/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 dpfpl 8 0.2 1 1
+
+# Task 94/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 promptfl 8 0.2 1 1
+
+# Task 96/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 sepfpl 8 0.2 1 1
+
+# Task 98/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedpgp 8 0.2 1 1
+
+# Task 100/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedotp 8 0.2 1 1
+
+# Task 102/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/caltech-101.yaml 10 dpfpl 8 0.4 1 1
+
+# Task 104/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/caltech-101.yaml 10 promptfl 8 0.4 1 1
+
+# Task 106/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 sepfpl 8 0.4 1 1
+
+# Task 108/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 fedpgp 8 0.4 1 1
+
+# Task 110/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 fedotp 8 0.4 1 1
+
+# Task 112/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 dpfpl 8 0.4 1 1
+
+# Task 114/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 promptfl 8 0.4 1 1
+
+# Task 116/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 sepfpl 8 0.4 1 1
+
+# Task 118/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedpgp 8 0.4 1 1
+
+# Task 120/120
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedotp 8 0.4 1 1
+
