1fa96215db2dcf36404a60bcc946f06e8f7b2062
Anonymeee <1712543411@qq.com>
2025-11-17 15:25:50 +0800

    update

--- tasks/terminal_0.sh
+++ tasks/terminal_0.sh
@@ -1,100 +1,75 @@
 #!/bin/bash
-# Terminal 1 tasks (GPU 0) - Total: 36 tasks
+# Terminal 1 tasks (GPU 1) - Total: 24 tasks
 
+# Task 1/24
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 promptfl 8 0.0 1 noniid-labeldir 30 1
 
-# Task 9/72
-CUDA_VISIBLE_DEVICES=0 bash srun_main.sh /home/liuxin25/dataset configs/datasets/caltech-101.yaml 10 promptfl 8 0.2 1 noniid-labeldir 30 0
+# Task 2/24
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedotp 8 0.0 1 noniid-labeldir 30 1
 
-# Task 11/72
-CUDA_VISIBLE_DEVICES=0 bash srun_main.sh /home/liuxin25/dataset configs/datasets/caltech-101.yaml 10 fedpgp 8 0.2 1 noniid-labeldir 30 0
+# Task 3/24
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedpgp 8 0.0 1 noniid-labeldir 30 1
 
-# Task 13/72
-CUDA_VISIBLE_DEVICES=0 bash srun_main.sh /home/liuxin25/dataset configs/datasets/caltech-101.yaml 10 promptfl 8 0.1 1 noniid-labeldir 30 0
+# Task 4/24
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 dpfpl 8 0.0 1 noniid-labeldir 30 1
 
-# Task 15/72
-CUDA_VISIBLE_DEVICES=0 bash srun_main.sh /home/liuxin25/dataset configs/datasets/caltech-101.yaml 10 fedpgp 8 0.1 1 noniid-labeldir 30 0
+# Task 5/24
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 promptfl 8 0.4 1 noniid-labeldir 30 1
 
-# Task 17/72
-CUDA_VISIBLE_DEVICES=0 bash srun_main.sh /home/liuxin25/dataset configs/datasets/caltech-101.yaml 10 promptfl 8 0.05 1 noniid-labeldir 30 0
+# Task 6/24
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedotp 8 0.4 1 noniid-labeldir 30 1
 
-# Task 19/72
-CUDA_VISIBLE_DEVICES=0 bash srun_main.sh /home/liuxin25/dataset configs/datasets/caltech-101.yaml 10 fedpgp 8 0.05 1 noniid-labeldir 30 0
+# Task 7/24
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedpgp 8 0.4 1 noniid-labeldir 30 1
 
-# Task 21/72
-CUDA_VISIBLE_DEVICES=0 bash srun_main.sh /home/liuxin25/dataset configs/datasets/caltech-101.yaml 10 promptfl 8 0.01 1 noniid-labeldir 30 0
+# Task 8/24
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 dpfpl 8 0.4 1 noniid-labeldir 30 1
 
-# Task 23/72
-CUDA_VISIBLE_DEVICES=0 bash srun_main.sh /home/liuxin25/dataset configs/datasets/caltech-101.yaml 10 fedpgp 8 0.01 1 noniid-labeldir 30 0
+# Task 9/24
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 promptfl 8 0.2 1 noniid-labeldir 30 1
 
-# Task 25/72
-CUDA_VISIBLE_DEVICES=0 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 promptfl 8 0.0 1 noniid-labeldir 30 0
+# Task 10/24
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedotp 8 0.2 1 noniid-labeldir 30 1
 
-# Task 27/72
-CUDA_VISIBLE_DEVICES=0 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 fedpgp 8 0.0 1 noniid-labeldir 30 0
+# Task 11/24
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedpgp 8 0.2 1 noniid-labeldir 30 1
 
-# Task 29/72
-CUDA_VISIBLE_DEVICES=0 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 promptfl 8 0.4 1 noniid-labeldir 30 0
+# Task 12/24
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 dpfpl 8 0.2 1 noniid-labeldir 30 1
 
-# Task 31/72
-CUDA_VISIBLE_DEVICES=0 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 fedpgp 8 0.4 1 noniid-labeldir 30 0
+# Task 13/24
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 promptfl 8 0.1 1 noniid-labeldir 30 1
 
-# Task 33/72
-CUDA_VISIBLE_DEVICES=0 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 promptfl 8 0.2 1 noniid-labeldir 30 0
+# Task 14/24
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedotp 8 0.1 1 noniid-labeldir 30 1
 
-# Task 35/72
-CUDA_VISIBLE_DEVICES=0 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 fedpgp 8 0.2 1 noniid-labeldir 30 0
+# Task 15/24
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedpgp 8 0.1 1 noniid-labeldir 30 1
 
-# Task 37/72
-CUDA_VISIBLE_DEVICES=0 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 promptfl 8 0.1 1 noniid-labeldir 30 0
+# Task 16/24
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 dpfpl 8 0.1 1 noniid-labeldir 30 1
 
-# Task 39/72
-CUDA_VISIBLE_DEVICES=0 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 fedpgp 8 0.1 1 noniid-labeldir 30 0
+# Task 17/24
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 promptfl 8 0.05 1 noniid-labeldir 30 1
 
-# Task 41/72
-CUDA_VISIBLE_DEVICES=0 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 promptfl 8 0.05 1 noniid-labeldir 30 0
+# Task 18/24
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedotp 8 0.05 1 noniid-labeldir 30 1
 
-# Task 43/72
-CUDA_VISIBLE_DEVICES=0 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 fedpgp 8 0.05 1 noniid-labeldir 30 0
+# Task 19/24
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedpgp 8 0.05 1 noniid-labeldir 30 1
 
-# Task 45/72
-CUDA_VISIBLE_DEVICES=0 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 promptfl 8 0.01 1 noniid-labeldir 30 0
+# Task 20/24
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 dpfpl 8 0.05 1 noniid-labeldir 30 1
 
-# Task 47/72
-CUDA_VISIBLE_DEVICES=0 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_pets.yaml 10 fedpgp 8 0.01 1 noniid-labeldir 30 0
+# Task 21/24
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 promptfl 8 0.01 1 noniid-labeldir 30 1
 
-# Task 49/72
-CUDA_VISIBLE_DEVICES=0 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 promptfl 8 0.0 1 noniid-labeldir 30 0
+# Task 22/24
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedotp 8 0.01 1 noniid-labeldir 30 1
 
-# Task 51/72
-CUDA_VISIBLE_DEVICES=0 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 fedpgp 8 0.0 1 noniid-labeldir 30 0
+# Task 23/24
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 fedpgp 8 0.01 1 noniid-labeldir 30 1
 
-# Task 53/72
-CUDA_VISIBLE_DEVICES=0 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 promptfl 8 0.4 1 noniid-labeldir 30 0
-
-# Task 55/72
-CUDA_VISIBLE_DEVICES=0 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 fedpgp 8 0.4 1 noniid-labeldir 30 0
-
-# Task 57/72
-CUDA_VISIBLE_DEVICES=0 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 promptfl 8 0.2 1 noniid-labeldir 30 0
-
-# Task 59/72
-CUDA_VISIBLE_DEVICES=0 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 fedpgp 8 0.2 1 noniid-labeldir 30 0
-
-# Task 61/72
-CUDA_VISIBLE_DEVICES=0 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 promptfl 8 0.1 1 noniid-labeldir 30 0
-
-# Task 63/72
-CUDA_VISIBLE_DEVICES=0 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 fedpgp 8 0.1 1 noniid-labeldir 30 0
-
-# Task 65/72
-CUDA_VISIBLE_DEVICES=0 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 promptfl 8 0.05 1 noniid-labeldir 30 0
-
-# Task 67/72
-CUDA_VISIBLE_DEVICES=0 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 fedpgp 8 0.05 1 noniid-labeldir 30 0
-
-# Task 69/72
-CUDA_VISIBLE_DEVICES=0 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 promptfl 8 0.01 1 noniid-labeldir 30 0
-
-# Task 71/72
-CUDA_VISIBLE_DEVICES=0 bash srun_main.sh /home/liuxin25/dataset configs/datasets/oxford_flowers.yaml 10 fedpgp 8 0.01 1 noniid-labeldir 30 0
+# Task 24/24
+CUDA_VISIBLE_DEVICES=1 bash srun_main.sh /home/liuxin25/dataset configs/datasets/food-101.yaml 10 dpfpl 8 0.01 1 noniid-labeldir 30 1
 
